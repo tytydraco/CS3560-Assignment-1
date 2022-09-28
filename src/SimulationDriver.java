c8ac87e28fc216d4eedc2dd63b2a676c3ebc8db8
Tyler Nijmeh <tylernij@gmail.com>
2022-09-27 18:51:15 -0700

    Multiple choices per student + configurable students
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- src/SimulationDriver.java
+++ src/SimulationDriver.java
@@ -1,7 +1,13 @@
 import java.util.Random;
 
 public class SimulationDriver {
+    /**
+     * The number of students to use per simulation test.
+     */
+    static final int numberOfStudents = 100;
+
     public static void main(String[] args) {
+        // Do a multiple choice test.
         System.out.println("Multiple choice test:");
         Question demoMCQuestion = new MultipleChoiceQuestion(
                 "Here is an example multiple-choice question!",
@@ -9,8 +15,10 @@ public class SimulationDriver {
         );
         testQuestion(demoMCQuestion);
 
+        // Space between tests.
         System.out.println();
 
+        // Do a single choice test.
         System.out.println("Single choice test:");
         Question demoSingleQuestion = new SingleChoiceQuestion(
                 "Here is an example single-choice question!",
@@ -32,11 +40,17 @@ public class SimulationDriver {
         VotingService votingService = new VotingService(question);
 
         String[] candidateAnswers = question.candidateAnswers;
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < numberOfStudents; i++) {
             Student student = new Student(String.valueOf(i));
-            String answer1 = candidateAnswers[random.nextInt(candidateAnswers.length)];
-            String answer2 = candidateAnswers[random.nextInt(candidateAnswers.length)];
-            votingService.vote(student, new String[]{answer1, answer2});
+
+            // Allow students to vote for any random number of answers.
+            final int answerCount = random.nextInt(candidateAnswers.length - 1) + 1;
+            final String[] answers = new String[answerCount];
+            for (int answerIdx = 0; answerIdx < answerCount; answerIdx++) {
+                answers[answerIdx] = candidateAnswers[random.nextInt(candidateAnswers.length)];
+            }
+
+            votingService.vote(student, answers);
         }
 
         votingService.printStats();
